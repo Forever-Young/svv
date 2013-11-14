8156863a0172eb10b04effcd10890f0045bbafff
Anton Novosyolov <anton.novosyolov@gmail.com>
2013-11-14 10:25:49 +0400

    Fabfile

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+from fabric.api import *
+
+env.hosts = ['svv']
+env.use_ssh_config = True
+
+def deploy():
+    with cd('/home/svv/svv'):
+        print("Updating sources")
+        print("Checking for uncommitted changes")
+        with settings(hide('running', 'stdout', 'stderr', 'warnings'), warn_only=True):
+            result = run('hg shelve').return_code
+        if result == 0:
+            print("Shelved uncommitted changes")
+        else:
+            print("No uncommitted changes")
+        run('hg pull')
+        run('hg update').return_code
+
+        with prefix('source /home/svv/env/bin/activate'):
+            run('./manage.py syncdb --noinput')
+            run('./manage.py migrate --noinput')
+            run('./manage.py collectstatic -c -l --noinput')
+
+    print("Restarting workers")
+    run('sudo /home/svv/restart-wsgi.sh')
+    run('sudo /home/svv/restart-celery.sh')
