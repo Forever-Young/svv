dca3cdae4f7123d866c2c4e2cb9bfa9b344aec82
Anton Novosyolov <anton.novosyolov@gmail.com>
2014-06-21 16:02:12 +0400

    Elasticsearch, use morphology analyzer

--- svv_project/settings.py
+++ svv_project/settings.py
@@ -121,11 +121,66 @@ CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost:5672//'
 
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'ENGINE': 'svv.haystack_backend.ConfigurableElasticSearchEngine',
         'URL': '127.0.0.1:9200',
         'INDEX_NAME': 'svv',
     },
 }
+
+ELASTICSEARCH_INDEX_SETTINGS = {
+    'settings': {
+        "analysis": {
+            "analyzer": {
+                "ngram_analyzer": {
+                    "type": "custom",
+                    "tokenizer": "lowercase",
+                    "filter": ["haystack_ngram"]
+                },
+                "edgengram_analyzer": {
+                    "type": "custom",
+                    "tokenizer": "lowercase",
+                    "filter": ["haystack_edgengram"]
+                },
+                "my_analyzer": {
+                    "type": "custom",
+                    "tokenizer": "standard",
+                    "filter": ["lowercase", "russian_morphology", "english_morphology", "my_stopwords"]
+                }
+            },
+            "tokenizer": {
+                "haystack_ngram_tokenizer": {
+                    "type": "nGram",
+                    "min_gram": 3,
+                    "max_gram": 15,
+                },
+                "haystack_edgengram_tokenizer": {
+                    "type": "edgeNGram",
+                    "min_gram": 2,
+                    "max_gram": 15,
+                    "side": "front"
+                }
+            },
+            "filter": {
+                "haystack_ngram": {
+                    "type": "nGram",
+                    "min_gram": 3,
+                    "max_gram": 15
+                },
+                "haystack_edgengram": {
+                    "type": "edgeNGram",
+                    "min_gram": 2,
+                    "max_gram": 15
+                },
+                "my_stopwords": {
+                    "type": "stop",
+                    "stopwords": "а,без,более,бы,был,была,были,было,быть,в,вам,вас,весь,во,вот,все,всего,всех,вы,где,да,даже,для,до,его,ее,если,есть,еще,же,за,здесь,и,из,или,им,их,к,как,ко,когда,кто,ли,либо,мне,может,мы,на,надо,наш,не,него,нее,нет,ни,них,но,ну,о,об,однако,он,она,они,оно,от,очень,по,под,при,с,со,так,также,такой,там,те,тем,то,того,тоже,той,только,том,ты,у,уже,хотя,чего,чей,чем,что,чтобы,чье,чья,эта,эти,это,я,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,no,not,of,on,or,such,that,the,their,then,there,these,they,this,to,was,will,with"
+                }
+            }
+        }
+    }
+}
+
+ELASTICSEARCH_DEFAULT_ANALYZER = "my_analyzer"
 
 ISSUES_PER_PAGE = HAYSTACK_SEARCH_RESULTS_PER_PAGE = 6
 SPEEDUP = "+20"
