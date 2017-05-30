16586ea0ee907f1dfba29e757a535c78bba71e57
Anton Novosyolov <anton.novosyolov@gmail.com>
2017-05-30 13:50:03 +0300

    Remove skip feed mark action

--- svv/admin.py
+++ svv/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from .models import PodcastIssue
 
 
+def remove_skip_feed(modeladmin, request, queryset):
+    for issue in queryset:
+        issue.skip_feed = False
+        issue.save()
+remove_skip_feed.short_description = 'Remove skip feed mark from selected issues'
+
+
 class PodcastIssueAdmin(admin.ModelAdmin):
     list_display = ('title', 'views', 'last_view', 'downloaded', 'skip_feed')
     search_fields = ('title', 'youtube_url')
@@ -10,5 +17,7 @@ class PodcastIssueAdmin(admin.ModelAdmin):
     def downloaded(self, obj):
         return bool(obj.file)
     downloaded.boolean = True
+
+    actions = [remove_skip_feed]
 
 admin.site.register(PodcastIssue, PodcastIssueAdmin)
